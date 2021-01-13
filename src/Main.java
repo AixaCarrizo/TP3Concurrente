fe34a8050ef898ff002dc88c7983f43c0830b61e
Lucas <lucasmttrabjo@gmail.com>
2021-01-12 21:04:19 -0300

    Pruebita. UwU

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main{
 
     public static void main(String[] args) {
 
+
+        /**
+         * ESTO ES UNA PRUEBA DE SI PUEDO HACER PUSH.
+         */
+
+
+
         CPU_buffer buffer1 = new CPU_buffer();
         CPU_buffer buffer2 = new CPU_buffer();
         
